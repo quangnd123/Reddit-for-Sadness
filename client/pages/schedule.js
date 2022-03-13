bd9787bb65c273e0f4f8ba420f4790de05ac692a
Eliezer Ong <ongeliezer99@gmail.com>
2022-03-13 23:45:25 +0800

    install semantic UI and added button

--- client/pages/schedule.js
+++ client/pages/schedule.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
 
 const schedule = () => {
-  return <div>schedule</div>;
+  return (
+    <div>
+      <button>Primary</button>
+    </div>
+  );
 };
 
 export default schedule;
