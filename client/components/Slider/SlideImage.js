a830f5a148f83c5398de1a959991152956f8f8c2
Eliezer Ong <ongeliezer99@gmail.com>
2022-03-23 19:42:26 +0800

    changed css to format slideshow

--- client/components/Slider/SlideImage.js
+++ client/components/Slider/SlideImage.js
@@ -1,13 +1,22 @@
 import React from "react";
 import styles from "./Slider.module.css";
+import {
+  Button,
+  Container,
+  Image,
+  Divider,
+  Form,
+  Header,
+} from "semantic-ui-react";
 
-function SlideImage({ image, content, url }) {
+function SlideImage({ image, content, url, info }) {
   return (
-    <div className={styles.imgcontainer}>
+    <div>
+      <div className={styles.wordPositioning}>{info}</div>
       <a href={url} target="_blank">
         <div className={styles.positioning}>{content}</div>
       </a>
-      <img src={image} className={styles.image} alt="mental health"></img>
+      <img src={image} className={styles.image} alt="mental health" />
     </div>
   );
 }
--- client/components/Slider/dataSlider.js
+++ client/components/Slider/dataSlider.js
@@ -4,18 +4,21 @@ const dataSlider = [
       "https://www.elumatherapy.com/wp-content/uploads/2022/02/Importance-of-Mental-Health_eLuma.png",
     url: "https://www.helpguide.org/articles/depression/coping-with-depression.htm",
     content: "Click here to find out more about depression!",
+    info: "It's ok to not be ok",
   },
   {
     image:
       "https://www.ucalgary.ca/risk/sites/default/files/styles/ucws_hero_cta_desktop/public/2020-11/20-ADV-1553006-UFlourish-Web-Hero2-1500x600.jpg?itok=zJtUU5jG",
     url: "https://www.helpguide.org/home-pages/anxiety.htm",
     content: "Click here to find out more about anxiety!",
+    info: "It's ok to not be ok",
   },
   {
     image:
       "https://nursing.ucalgary.ca/sites/default/files/styles/ucws_hero_cta_desktop/public/2019-10/19-NUR-98527-Nursing%20Mental%20Health%20Graphic_final-Master-1500x800.jpg?h=c7249836&itok=06pWeydR",
     url: "https://www.samhealth.org.sg/",
     content: "Click here to find out more about other mental health illnesses!",
+    info: "It's ok to not be ok",
   },
 ];
 
