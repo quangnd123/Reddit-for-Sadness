bf47c1bcafdffee3d414fd7599df84ee649b0240
Eliezer Ong <ongeliezer99@gmail.com>
2022-03-23 17:08:57 +0800

    changed images

--- client/components/Slider/dataSlider.js
+++ client/components/Slider/dataSlider.js
@@ -1,19 +1,19 @@
 const dataSlider = [
   {
     image:
-      "https://images.unsplash.com/photo-1579600161224-cac5a2971069?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8bWVudGFsJTIwaGVhbHRofGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWt5-5KLPO7gDHwxnQtpiC_AWsrAP6ZxjmWg&usqp=CAU",
     url: "https://www.helpguide.org/articles/depression/coping-with-depression.htm",
     content: "Click here to find out more about depression!",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1569437061241-a848be43cc82?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1lbnRhbCUyMGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF-mWgwPtQ5ibViffYGqfoMmvdhmSy5g-P5Q&usqp=CAU",
     url: "https://www.helpguide.org/home-pages/anxiety.htm",
     content: "Click here to find out more about anxiety!",
   },
   {
     image:
-      "https://images.unsplash.com/photo-1579208575657-c595a05383b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fG1lbnRhbCUyMGhlYWx0aHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+      "https://www.healthshots.com/wp-content/uploads/2020/07/good-mental-health-370x207.jpg",
     url: "https://www.samhealth.org.sg/",
     content: "Click here to find out more about other mental health illnesses!",
   },
