4500d7dacf71852fbd7bb4bfce2456b81ee701a7
Eliezer Ong <ongeliezer99@gmail.com>
2022-03-26 15:23:36 +0800

    added survey component

--- client/components/Survey/ModalSurvey.js
+++ client/components/Survey/ModalSurvey.js
@@ -8,6 +8,7 @@ import {
 } from "semantic-ui-react";
 import styles from "./ModalSurvey.module.css";
 import React, { useState, useContext } from "react";
+import Survey from "./Survey";
 
 function ModalSurvey() {
   const [open, setOpen] = useState(false);
@@ -24,126 +25,11 @@ function ModalSurvey() {
       <Modal.Content>
         <Header as="h2">We would like to get to know you!</Header>
         <Form>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q1" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q1" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q1" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q1" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q1" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q1" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q1" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q1" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q1" className={styles.radio} />9
-          </Form.Field>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q2" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q2" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q2" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q2" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q2" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q2" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q2" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q2" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q2" className={styles.radio} />9
-          </Form.Field>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q3" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q3" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q3" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q3" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q3" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q3" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q3" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q3" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q3" className={styles.radio} />9
-          </Form.Field>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q4" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q4" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q4" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q4" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q4" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q4" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q4" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q4" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q4" className={styles.radio} />9
-          </Form.Field>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q5" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q5" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q5" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q5" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q5" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q5" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q5" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q5" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q5" className={styles.radio} />9
-          </Form.Field>
-          <Form.Field>
-            <label className={styles.label}>Are you gay?</label>
-            <input type="radio" value="1" name="Q6" className={styles.radio} />
-            1
-            <input type="radio" value="2" name="Q6" className={styles.radio} />
-            2
-            <input type="radio" value="3" name="Q6" className={styles.radio} />
-            3
-            <input type="radio" value="4" name="Q6" className={styles.radio} />
-            4
-            <input type="radio" value="5" name="Q6" className={styles.radio} />
-            5
-            <input type="radio" value="6" name="Q6" className={styles.radio} />
-            6
-            <input type="radio" value="7" name="Q6" className={styles.radio} />
-            7
-            <input type="radio" value="8" name="Q6" className={styles.radio} />
-            8
-            <input type="radio" value="9" name="Q6" className={styles.radio} />9
-          </Form.Field>
+          <Survey question={"Are you gay?"} label={"Q1"} />
+          <Survey question={"Are you gay?"} label={"Q2"} />
+          <Survey question={"Are you gay?"} label={"Q3"} />
+          <Survey question={"Are you gay?"} label={"Q4"} />
+          <Survey question={"Are you gay?"} label={"Q5"} />
           <div className={styles.submit}>
             <Modal.Actions>
               <Button
